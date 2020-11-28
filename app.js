c2770faa6187a301ff00c33447fada1bfe53faf6
ammer-ahmad <ameer5ahmad@gmail.com>
2020-11-28 14:36:13 -0500

    added functionality to change sounds

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ class DrumKit {
     constructor() {
         this.pads = document.querySelectorAll(".pad");
         this.playBtn = document.querySelector(".play");
+        this.currentKick = "./allSounds/kick-classic.wav";
+        this.currentSnare = "./allSounds/snare-acoustic01.wav.wav";
+        this.currentHihat = "./allSounds/hihat-acoustic01.wav.wav";
         this.kickAudio = document.querySelector(".kick-sound");
         this.snareAudio = document.querySelector(".snare-sound");
         this.hihatAudio = document.querySelector(".hihat-sound");
         this.index = 0;
         this.bpm = 150;
         this.isPlaying = null;
+        this.selects = document.querySelectorAll("select");
     }
     activePad() {
         this.classList.toggle("active");
@@ -57,9 +61,26 @@ class DrumKit {
             this.playBtn.classList.remove("active");
         }
     }
+    changeSound(e) {
+        const selectionName = e.target.name;
+        const selectionValue = e.target.value;
+        switch (selectionName) {
+            case "kick-select":
+                this.kickAudio.src = selectionValue;
+                break;
+            case "snare-select":
+                this.snareAudio.src = selectionValue;
+                break;
+            case "hihat-select":
+                this.hihatAudio.src = selectionValue;
+                break;
+        }
+    }
 }
 
 const drumKit = new DrumKit();
+
+//Event Listeners
 
 drumKit.pads.forEach((pad) => {
     pad.addEventListener("click", drumKit.activePad);
@@ -71,4 +92,10 @@ drumKit.pads.forEach((pad) => {
 drumKit.playBtn.addEventListener("click", () => {
     drumKit.updateBtn();
     drumKit.start();
+});
+
+drumKit.selects.forEach((select) => {
+    select.addEventListener("change", function(e) {
+        drumKit.changeSound(e);
+    });
 });
